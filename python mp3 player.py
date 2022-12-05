51c09bd4118ef4444715865d70938d932d26b9d0
mohbaba <babamu09@gmail.com>
2022-12-05 23:39:51 +0100

    Playlist play  function updated

--- python mp3 player.py
+++ python mp3 player.py
@@ -1,23 +1,30 @@
 from pydub import AudioSegment
 from pydub.playback import play
+from pygame import mixer
 import pygame
 import os
 
-pygame.init()
-pygame.mixer.init()
+# pygame.init()
+mixer.init()
 playlist = "C:/Users/hp/Desktop/gbedu/"
 playlist_list = os.listdir(playlist)
-
+os.chdir(playlist)
 print(playlist_list)
 # play(new_song)
 # clean_audio = []
 def playlist_player():
-    os.chdir(playlist)
-    for files in playlist_list:
-        exported_files = AudioSegment.from_file(files)
-        song_start = exported_files[:10*1000]
-        play(song_start)
-
+    for file in playlist_list:
+        # exported_files = AudioSegment.from_file(file)
+        mixer.music.load(file)
+        mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    # os.chdir(playlist)
+    # for files in playlist_list:
+        # exported_files = AudioSegment.from_file(files)
+        # song_start = exported_files[:10*1000]
+        # play(song_start)
+playlist_player()
 def play_song():
     # this function plays the selected song from the song library or playlist
     pass 
