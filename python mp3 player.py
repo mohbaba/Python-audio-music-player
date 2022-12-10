25740609b20459f1b126befff351f53ed8e72c8f
mohbaba <babamu09@gmail.com>
2022-12-10 21:44:38 +0100

    Previous function

--- python mp3 player.py
+++ python mp3 player.py
@@ -12,9 +12,7 @@ os.chdir(playlist)
 index = 0
 print(playlist_list)
 print(playlist_list.index('01 Drake - Tuscan Leather.mp3'))
-# file = '01 - Starboy (feat. Daft Punk).mp3'
-# mixer.music.load(file)
-# mixer.music.play()
+
 
 
 
@@ -23,18 +21,9 @@ def playlist_player():
     for file in playlist_list:
         mixer.music.load(file)
         mixer.music.play()
-        # query = input("Press 'p' to pause")
-        # if query == 'p':
-        #     mixer.music.pause()
         while mixer.music.get_busy():
-            pygame.time.Clock().tick(10)
-            pause_song()
-    
-    # os.chdir(playlist)
-    # for files in playlist_list:
-        # exported_files = AudioSegment.from_file(files)
-        # song_start = exported_files[:10*1000]
-        # play(song_start)
+                pygame.time.Clock().tick(10)
+                pause_song()
 
         
 
@@ -64,7 +53,7 @@ def play_song():
     #         mixer.music.stop()
             
                 
-playlist_player()
+
 # play_song()
 def pause_song():
     while True:
@@ -73,17 +62,21 @@ def pause_song():
             mixer.music.pause()
         elif query == 'r':
             mixer.music.unpause()
-        elif query == 'e':
-            mixer.music.stop()
         elif query == 'n':
             global index
             index += 1
             mixer.music.load(playlist_list[index])
             mixer.music.play()
-                
+        elif query == 'q':
+            index -= 1
+            mixer.music.load(playlist_list[index])
+            mixer.music.play()
+        elif query == 'e':
+            mixer.music.stop()
+            break    
             
 # play_song()
-
+playlist_player()
 
 
 def play_next_song():
@@ -93,9 +86,19 @@ def play_next_song():
     mixer.music.load(playlist_list[index])
     mixer.music.play()
     
+    # Another way of moving to the next in the playlist is:
+    # index = 0
+    # num = playlist_list.index(file)
+    # index = num + 1
+    # mixer.music.load(playlist_list[index])
+    # mixer.music.play()
     pass
 
 def play_prev_song():
+    global index
+    index -= 1
+    mixer.music.load(playlist_list[index])
+    mixer.music.play()
     pass
 
 def repeat():
