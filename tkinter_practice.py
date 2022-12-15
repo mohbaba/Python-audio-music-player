dd414ee77e4a146997fbc61335702a843c2253c5
mohbaba <babamu09@gmail.com>
2022-12-15 07:45:41 +0100

    practice

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -3,6 +3,9 @@ from tkinter import *
 root = Tk()
 
 myLabel = Label(root , text = "Hello world") #Label takes in two arguements, where the widget should be located and the text it will comprise of.
-myLabel.pack()
+myLabel2 = Label(root, text = "What's up my people?")
+
+myLabel.grid(row = 0, column= 0)
+myLabel2.grid(row= 0, column= 1)
 
 root.mainloop()
