e260cd085044826454649c49850197bee877a6f7
mohbaba <babamu09@gmail.com>
2022-12-21 16:26:18 +0100

    Addition button

--- tkinter_calculator.py
+++ tkinter_calculator.py
@@ -5,25 +5,45 @@ root.title('Simple Calculator' )
 
 e = Entry(root, width = 35,borderwidth=5 )
 e.grid(row =0,column =0, columnspan = 3, padx = 10, pady = 10)
-e.insert(0,"")
 
-def button_add():
-    pass
 
-button_1 = Button(root, text='1',padx = 40, pady = 30,command = button_add)
-button_2 = Button(root, text='2',padx = 40, pady = 30,command = button_add)
-button_3 = Button(root, text='3',padx = 40, pady = 30,command = button_add)
-button_4 = Button(root, text='4',padx = 40, pady = 30,command = button_add)
-button_5 = Button(root, text='5',padx = 40, pady = 30,command = button_add)
-button_6 = Button(root, text='6',padx = 40, pady = 30,command = button_add)
-button_7 = Button(root, text='7',padx = 40, pady = 30,command = button_add)
-button_8 = Button(root, text='8',padx = 40, pady = 30,command = button_add)
-button_9 = Button(root, text='9',padx = 40, pady = 30,command = button_add)
-button_0 = Button(root, text='0',padx = 40, pady = 30,command = button_add)
-button_add = Button(root, text='+',padx =84, pady = 30 )
-button_equals = Button(root, text='=',padx =39, pady = 30)
+def button_add(number):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+    
+def clear():
+    e.delete(0, END)
+
+def addition():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    
+def equal():
+    second_number = e.get
+    s_num = int(second_number)
+    result = f_num + s_num
+    e.delete(0,END)
+    e.insert(0,result)
+    
+    
+
+button_1 = Button(root, text='1',padx = 40, pady = 30,command = lambda: button_add(1))
+button_2 = Button(root, text='2',padx = 40, pady = 30,command = lambda: button_add(2))
+button_3 = Button(root, text='3',padx = 40, pady = 30,command = lambda: button_add(3))
+button_4 = Button(root, text='4',padx = 40, pady = 30,command =lambda: button_add(4))
+button_5 = Button(root, text='5',padx = 40, pady = 30,command = lambda:button_add(5))
+button_6 = Button(root, text='6',padx = 40, pady = 30,command =lambda: button_add(6))
+button_7 = Button(root, text='7',padx = 40, pady = 30,command = lambda:button_add(7))
+button_8 = Button(root, text='8',padx = 40, pady = 30,command = lambda:button_add(8))
+button_9 = Button(root, text='9',padx = 40, pady = 30,command = lambda:button_add(9))
+button_0 = Button(root, text='0',padx = 40, pady = 30,command = lambda:button_add(0))
+add_button = Button(root, text='+',padx =84, pady = 30 , command=addition)
+button_equals = Button(root, text='=',padx =39, pady = 30 , command= equal)
 button_times = Button(root, text='x',padx =39, pady = 30)
-button_clear = Button(root, text='Clear',padx =77, pady = 30)
+button_clear = Button(root, text='Clear',padx =77, pady = 30, command = clear)
 
 
 button_1.grid(row =3, column =0 )
@@ -40,7 +60,7 @@ button_9.grid(row = 1, column =2 )
 
 button_0.grid(row =4 , column =0 )
 
-button_add.grid(row = 4, column = 1, columnspan = 2)
+add_button.grid(row = 4, column = 1, columnspan = 2)
 button_equals.grid(row = 5, column = 0)
 button_clear.grid(row = 5, column = 1 , columnspan = 2)
 
